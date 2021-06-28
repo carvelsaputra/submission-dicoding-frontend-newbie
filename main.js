e64e7bc81edc0af75f6e67ae1819c23d76eaab03
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-06-28 13:09:00 +0700

    fix bug on insert

--- main.js
+++ main.js
@@ -8,15 +8,17 @@ let dataBuku = {
 
 const storageKey = "STORAGE_KEY";
 const submitAction = document.getElementById("inputBook");
+
 function checkForStorage() {
   return typeof Storage !== "undefined";
 }
+
 function storeBook() {
   dataBuku.id = new Date().getTime();
   dataBuku.judul = document.getElementById("inputBookTitle").value;
   dataBuku.penulis = document.getElementById("inputBookAuthor").value;
   dataBuku.tahun = document.getElementById("inputBookYear").value;
-  dataBuku.statusBaca = document.getElementById("inputBookIsComplete").value;
+  dataBuku.statusBaca = document.getElementById("inputBookIsComplete").checked;
   if (!checkForStorage()) {
     return alert("Browser tidak support Web Storage !");
   }
@@ -25,7 +27,6 @@ function storeBook() {
     list = [];
   } else {
     list = JSON.parse(localStorage.getItem(storageKey));
-    console.log(list);
   }
   list.unshift(dataBuku);
   if (list.length > 5) {
@@ -33,8 +34,39 @@ function storeBook() {
   }
   localStorage.setItem(storageKey, JSON.stringify(list));
 }
-function getBookList() {
-  if (!checkForStorage()) {
-    return alert("Browser tidak support Web Storage !");
-  }
-}
+
+// function getBookList() {
+//   if (!checkForStorage()) {
+//     return [];
+//   }
+//   return JSON.parse(localStorage.getItem(storageKey));
+// }
+
+// function renderBookList() {
+//   const books = getBookList();
+//   console.log(books);
+//   const unCompleteBook = document.getElementById("#incompleteBookshelfList");
+//   const completeBook = document.getElementById("#completeBookshelfList");
+
+//   bookList.innerHTML = "";
+
+//   // <h3>Book Title</h3>
+//   // <p>Penulis: John Doe</p>
+//   // <p>Tahun: 2002</p>
+
+//   // <div class="action">
+//   //     <button class="green">Selesai dibaca</button>
+//   //     <button class="red">Hapus buku</button>
+//   // </div>
+
+//   for (let book of books) {
+//     let article = document.createElement("article");
+//     article.innerHTML = `
+//     <h3> ${book.nama} </h3>
+//     <p> Penulis : ${book.penulis} </p>
+//     <p> Tahun : ${book.tahun}</p>
+//     `;
+//     // if(book.)
+//     // bookList.appendChild(article);
+//   }
+// }
