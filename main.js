8fa2bb4c0bddfbf0208251665aa4740c0e067ff7
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-06-28 13:32:12 +0700

    refactor method insert

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-let dataBuku = {
-  id: "",
-  judul: "",
-  penulis: "",
-  tahun: "",
-  statusBaca: false,
-};
-
 const storageKey = "STORAGE_KEY";
 const submitAction = document.getElementById("inputBook");
 
@@ -13,12 +5,7 @@ function checkForStorage() {
   return typeof Storage !== "undefined";
 }
 
-function storeBook() {
-  dataBuku.id = new Date().getTime();
-  dataBuku.judul = document.getElementById("inputBookTitle").value;
-  dataBuku.penulis = document.getElementById("inputBookAuthor").value;
-  dataBuku.tahun = document.getElementById("inputBookYear").value;
-  dataBuku.statusBaca = document.getElementById("inputBookIsComplete").checked;
+function storeBook(dataBuku) {
   if (!checkForStorage()) {
     return alert("Browser tidak support Web Storage !");
   }
@@ -35,38 +22,58 @@ function storeBook() {
   localStorage.setItem(storageKey, JSON.stringify(list));
 }
 
-// function getBookList() {
-//   if (!checkForStorage()) {
-//     return [];
-//   }
-//   return JSON.parse(localStorage.getItem(storageKey));
-// }
+function getBookList() {
+  if (!checkForStorage()) {
+    return [];
+  }
+  return JSON.parse(localStorage.getItem(storageKey));
+}
 
-// function renderBookList() {
-//   const books = getBookList();
-//   console.log(books);
-//   const unCompleteBook = document.getElementById("#incompleteBookshelfList");
-//   const completeBook = document.getElementById("#completeBookshelfList");
+function renderBookList() {
+  const books = getBookList();
+  console.log(books);
+  const unCompleteBook = document.getElementById("#incompleteBookshelfList");
+  const completeBook = document.getElementById("#completeBookshelfList");
 
-//   bookList.innerHTML = "";
+  bookList.innerHTML = "";
 
-//   // <h3>Book Title</h3>
-//   // <p>Penulis: John Doe</p>
-//   // <p>Tahun: 2002</p>
+  // <h3>Book Title</h3>
+  // <p>Penulis: John Doe</p>
+  // <p>Tahun: 2002</p>
 
-//   // <div class="action">
-//   //     <button class="green">Selesai dibaca</button>
-//   //     <button class="red">Hapus buku</button>
-//   // </div>
+  // <div class="action">
+  //     <button class="green">Selesai dibaca</button>
+  //     <button class="red">Hapus buku</button>
+  // </div>
 
-//   for (let book of books) {
-//     let article = document.createElement("article");
-//     article.innerHTML = `
-//     <h3> ${book.nama} </h3>
-//     <p> Penulis : ${book.penulis} </p>
-//     <p> Tahun : ${book.tahun}</p>
-//     `;
-//     // if(book.)
-//     // bookList.appendChild(article);
-//   }
-// }
+  for (let book of books) {
+    let article = document.createElement("article");
+    article.innerHTML = `
+    <h3> ${book.nama} </h3>
+    <p> Penulis : ${book.penulis} </p>
+    <p> Tahun : ${book.tahun}</p>
+    `;
+
+    if (!book.statusBaca) return unCompleteBook.appendChild(article);
+
+    completeBook.appendChild(article);
+  }
+}
+
+submitAction.addEventListener("submit", function (event) {
+  const dataBuku = {
+    id: "",
+    judul: "",
+    penulis: "",
+    tahun: "",
+    statusBaca: false,
+  };
+
+  dataBuku.id = new Date().getTime();
+  dataBuku.judul = document.getElementById("inputBookTitle").value;
+  dataBuku.penulis = document.getElementById("inputBookAuthor").value;
+  dataBuku.tahun = document.getElementById("inputBookYear").value;
+  dataBuku.statusBaca = document.getElementById("inputBookIsComplete").checked;
+  storeBook(dataBuku);
+  renderBookList();
+});
